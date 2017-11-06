005ba75a27a99ad942773234bebe49619b6fedea
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2017-11-06 23:10:27 +1000

    Update version info.

--- lib/attr_array/version.rb
+++ lib/attr_array/version.rb
@@ -8,8 +8,8 @@ module AttrArray
     Summary     = "AttrArray v#{Compact}"
     Description = "A high performance ActiveRecord concern for Rails using the PostgreSQL array type."
     Author      = "Jurgen Jocubeit"
-    Email       = "hello@apikits.com"
-    Homepage    = "https://www.apikits.com"
+    Email       = "support@brightcommerce.com"
+    Homepage    = "https://github.com/brightcommerce/attr_array"
     Metadata    = {'copyright' => 'Copyright 2017 Pocket Business, Co. All Rights Reserved.'}
     License     = "UNLICENSED"
   end
